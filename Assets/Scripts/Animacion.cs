52d88906da338b560091d6b66b372ad0e0bba59d
dieguin545 <mariotaj@uninorte.edu.co>
2026-05-17 13:43:26 -0500

    Primer commit del proyecto

--- Assets/Scripts/Animacion.cs
+++ Assets/Scripts/Animacion.cs
@@ -2,12 +2,17 @@ using UnityEngine;
 
 public class Animacion : MonoBehaviour
 {
-public float velocidad = 5f;
+    public float velocidad = 5f;
     private Animator animator;
+    private Transform panelAccion;
+    private Vector3 escalaOriginalPanel;
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        panelAccion = transform.Find("PanelAccion");
+        if (panelAccion != null)
+            escalaOriginalPanel = panelAccion.localScale;
     }
 
     void Update()
@@ -16,7 +21,6 @@ public float velocidad = 5f;
         float movimientoY = Input.GetAxisRaw("Vertical");
 
         Vector3 movimiento = new Vector3(movimientoX, movimientoY, 0);
-
         transform.position += movimiento.normalized * velocidad * Time.deltaTime;
 
         bool caminando = movimiento != Vector3.zero;
@@ -25,10 +29,18 @@ public float velocidad = 5f;
         if (movimientoX > 0)
         {
             transform.localScale = new Vector3(27, 27, 27);
+            if (panelAccion != null)
+                panelAccion.localScale = escalaOriginalPanel;
         }
         else if (movimientoX < 0)
         {
             transform.localScale = new Vector3(-27, 27, 27);
+            if (panelAccion != null)
+                panelAccion.localScale = new Vector3(
+                    -escalaOriginalPanel.x,
+                    escalaOriginalPanel.y,
+                    escalaOriginalPanel.z
+                );
         }
     }
 }
